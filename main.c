6b7866f6a15348a989a52abe6980438cea5c5fba
firststef <firstbossspike@gmail.com>
2019-11-02 11:39:40 +0200

    Added myfind

--- main.c
+++ main.c
@@ -8,229 +8,377 @@
 #include <stdlib.h>
 #include <pwd.h>
 #include <limits.h>
+#include <dirent.h>
+#include <time.h>
+#include <sys/socket.h>
 
 # define PARSE_CONDITION(c) memcmp(input, c , strlen(c)) == 0
 #define READ 0
 #define WRITE 1
 
-void console_log(const char* str){
-    printf(">>>%s\n",str);
+void console_log(const char* str) {
+	printf(">>>%s\n", str);
 }
-void error_log(const char* str){
-    printf("ERROR: %s\n", str);
-}
-
-
-int login_func(char input[100], int pipe[2]){
-    FILE* fd = fopen("../users.txt","r");
-    if (fd == NULL){
-        close(pipe[READ]);
-        write(pipe[WRITE], "9:not found", strlen("9:not found"));
-        return -1;
-    }
-
-    char line[100];
-    bool found = false;
-    while (fgets(line, sizeof(line), fd)){
-        if (strcmp(input + strlen("login:"), line) == 0) {
-            close(pipe[READ]);
-            write(pipe[WRITE], "7:success", strlen("7:success"));
-
-            found = true;
-            break;
-        }
-    }
-    fclose(fd);
-
-    if (!found){
-        close(pipe[READ]);
-        write(pipe[WRITE], "6:failed", strlen("6:failed"));
-        return 0;
-    }
-
-    return 1;
+void error_log(const char* str) {
+	printf("ERROR: %s\n", str);
 }
 
-char* check_correct_size(char* msg){
-    char* st = strtok(msg,":");
-    char* end;
-    int len = strtol(msg, &end, 10);
-    if (len != strlen(st + strlen(st) + 1)){
-        return NULL;
-    }
-    return st + strlen(st) + 1;
+
+int login_func(char input[100], int pipe[2]) {
+	FILE* fd = fopen("../users.txt", "r");
+	if (fd == NULL) {
+		close(pipe[READ]);
+		write(pipe[WRITE], "9:not found", strlen("9:not found"));
+		return -1;
+	}
+
+	char line[100];
+	bool found = false;
+	while (fgets(line, sizeof(line), fd)) {
+		if (strcmp(input + strlen("login:"), line) == 0) {
+			close(pipe[READ]);
+			write(pipe[WRITE], "7:success", strlen("7:success"));
+
+			found = true;
+			break;
+		}
+	}
+	fclose(fd);
+
+	if (!found) {
+		close(pipe[READ]);
+		write(pipe[WRITE], "6:failed", strlen("6:failed"));
+		return 0;
+	}
+
+	return 1;
 }
 
-int mystat_func(char input[100], const char* fifo_name){
-    struct stat st;
-    char permisiuni[10]="---------";
+struct get_size {
+	int len;
+	char* newStart;
+};
 
-    FILE* fd = fopen(fifo_name, "w");
+struct get_size get_message_size(char* msg) {
+	struct get_size sc;
 
-    system("pwd");
-    if(0 != stat(input + strlen("mystat "),&st) )
-    {
-        fprintf(fd, "6:failed");
-        fclose(fd);
-        return -1;
-    }
+	char tmp[10];
+	strncpy(tmp, msg, 10);
+	char* st = strtok(tmp, ":");
+	char* end;
+	sc.len = strtol(st, &end, 10);
+	sc.newStart = msg + strlen(st) + 1;
 
-    fprintf(fd, "\nUID: %ld", (long) st.st_uid);
-    fprintf(fd, "\nGID: %ld", (long) st.st_gid);
-
-    fprintf(fd,"\nType: ");
-    switch( S_IFMT & st.st_mode )
-    {
-        case S_IFDIR : fprintf(fd,"Director"); break;
-        case S_IFREG : fprintf(fd,"File"); break;
-        case S_IFLNK : fprintf(fd,"Link"); break;
-        case S_IFIFO : fprintf(fd,"Fifo"); break;
-        case S_IFSOCK: fprintf(fd,"Socket"); break;
-        default: fprintf(fd,"Unknown file type");
-    }
-
-    fprintf(fd, "\nSize: %lld kb",(long long)st.st_size/1024);
-
-    if(S_IRUSR & st.st_mode) permisiuni[0]='r';
-    if(S_IWUSR & st.st_mode) permisiuni[1]='w';
-    if(S_IXUSR & st.st_mode) permisiuni[2]='x';
-    if(S_IRGRP & st.st_mode) permisiuni[3]='r';
-    if(S_IWGRP & st.st_mode) permisiuni[4]='w';
-    if(S_IXGRP & st.st_mode) permisiuni[5]='x';
-    if(S_IROTH & st.st_mode) permisiuni[6]='r';
-    if(S_IWOTH & st.st_mode) permisiuni[7]='w';
-    if(S_IXOTH & st.st_mode) permisiuni[8]='x';
-
-    fprintf(fd, "\nPermissions: %s", permisiuni);
-
-    fprintf(fd, "\n%s: %lld", "std_ino",st.st_ino);
-    fprintf(fd, "\n%s: %lld", "std_dev",st.st_dev);
-    fprintf(fd, "\n%s: %lld", "st_atim",st.st_atim);
-    fprintf(fd, "\n%s: %lld", "st_ctim",st.st_ctim);
-    fprintf(fd, "\n%s: %lld", "st_mtim",st.st_mtim);
-
-    fseek(fd, 0L, SEEK_END);
-    int sz = ftell(fd);
-    fseek(fd, 0L, SEEK_SET);
-
-    char temp_buffer[1000];
-    fread(temp_buffer, 1, sz, fd);
-
-    fseek(fd, 0L, SEEK_SET);
-    fprintf(fd, "%d:", sz);
-    fprintf(fd, "%s", temp_buffer);
-
-    fclose(fd);
-    return 1;
+	return sc;
 }
 
-int main( )
+int child_info_func(char* input, const char* fifo_name) {
+	struct stat st;
+	char permisiuni[10] = "---------";
+
+	int file = open(fifo_name, O_WRONLY);
+
+	char buffer[1000];
+
+	if (0 != stat(input, &st))
+	{
+		write(file, "6:failed", strlen("6:failed"));
+		close(file);
+		return -1;
+	}
+
+	sprintf(buffer, "UID: %ld", (long)st.st_uid);
+	sprintf(buffer + strlen(buffer), "\nGID: %ld", (long)st.st_gid);
+
+	sprintf(buffer + strlen(buffer), "\nType: ");
+	switch (S_IFMT & st.st_mode)
+	{
+	case S_IFDIR: sprintf(buffer + strlen(buffer), "Director"); break;
+	case S_IFREG: sprintf(buffer + strlen(buffer), "File"); break;
+	case S_IFLNK: sprintf(buffer + strlen(buffer), "Link"); break;
+	case S_IFIFO: sprintf(buffer + strlen(buffer), "Fifo"); break;
+	case S_IFSOCK: sprintf(buffer + strlen(buffer), "Socket"); break;
+	default: sprintf(buffer + strlen(buffer), "Unknown file type");
+	}
+
+	sprintf(buffer + strlen(buffer), "\nSize: %lld kb", (long long)st.st_size / 1024);
+
+	if (S_IRUSR & st.st_mode) permisiuni[0] = 'r';
+	if (S_IWUSR & st.st_mode) permisiuni[1] = 'w';
+	if (S_IXUSR & st.st_mode) permisiuni[2] = 'x';
+	if (S_IRGRP & st.st_mode) permisiuni[3] = 'r';
+	if (S_IWGRP & st.st_mode) permisiuni[4] = 'w';
+	if (S_IXGRP & st.st_mode) permisiuni[5] = 'x';
+	if (S_IROTH & st.st_mode) permisiuni[6] = 'r';
+	if (S_IWOTH & st.st_mode) permisiuni[7] = 'w';
+	if (S_IXOTH & st.st_mode) permisiuni[8] = 'x';
+
+	sprintf(buffer + strlen(buffer), "\nPermissions: %s", permisiuni);
+
+	sprintf(buffer + strlen(buffer), "\n%s: %lld", "std_ino", st.st_ino);
+	sprintf(buffer + strlen(buffer), "\n%s: %lld", "std_dev", st.st_dev);
+	sprintf(buffer + strlen(buffer), "\n%s: %lld", "st_atim", st.st_atim);
+	sprintf(buffer + strlen(buffer), "\n%s: %lld", "st_ctim", st.st_ctim);
+	sprintf(buffer + strlen(buffer), "\n%s: %lld", "st_mtim", st.st_mtim);
+
+	struct tm dt;
+
+	dt = *(gmtime(&st.st_ctime));
+	//sprintf(buffer + strlen(buffer),"\nCreated on: %d-%d-%d %d:%d:%d", dt.tm_mday, dt.tm_mon, dt.tm_year + 1900,dt.tm_hour, dt.tm_min, dt.tm_sec);
+
+	dt = *(gmtime(&st.st_mtime));
+	//sprintf(buffer + strlen(buffer),"\nModified on: %d-%d-%d %d:%d:%d", dt.tm_mday, dt.tm_mon, dt.tm_year + 1900,dt.tm_hour, dt.tm_min, dt.tm_sec);
+
+	int sz = strlen(buffer);
+
+	char number[10];
+	memset(number, '\0', 10);
+	sprintf(number, "%d", sz);
+
+	write(file, number, strlen(number));
+	write(file, ":", 1);
+	write(file, buffer, sz);
+
+	close(file);
+	console_log("Stat results:\n");
+	return 1;
+}
+
+bool is_dir(char* path) {
+	struct stat st;
+
+	if (0 != stat(path, &st))
+	{
+		return -1;
+	}
+
+	return (S_IFMT & st.st_mode) == S_IFDIR;
+}
+
+int get_info_func(char* input) {
+	printf("Stat function called with arg %s\n", input);
+
+	char path[100];
+	if (getcwd(path, sizeof(path)) == NULL) {
+		error_log("getcwd error");
+		return -1;
+	}
+
+	char* fifo_name = "fisier.txt";
+	strcat(path + strlen(path), "/");
+	strcat(path + strlen(path), fifo_name);
+	remove(path);
+
+	if (-1 == mkfifo(path, 0777)) {
+		error_log("Could not create fifo");
+		perror("Descr:");
+		return -1;
+	}
+	pid_t pid = fork();
+
+	if (pid == -1) {
+		error_log("Fork failed");
+		return 0;
+	}
+	else if (pid != 0) {
+		sleep(5);
+		int file = open(fifo_name, O_RDONLY);
+
+		char buffer[1000];
+		memset(buffer, '\0', 11);
+
+		read(file, buffer, 1000);
+
+		struct get_size size = get_message_size(buffer);
+		if (strlen(size.newStart) != size.len) {
+			error_log("Receive message not complete");
+			printf("%s len %d\n", size.newStart, strlen(size.newStart));
+			printf("%d", size.len);
+			close(file);
+			return -1;
+		}
+
+		if (memcmp(size.newStart, "failed", strlen("failed")) == 0) {
+			error_log("Stat function failed. Sanitisation of paths is required in some cases.");
+		}
+		else {
+			printf("%s\n", size.newStart);
+		}
+
+		close(file);
+
+		return 1;
+	}
+	else {
+		return child_info_func(input + strlen("mystat "), path);
+	}
+}
+
+int search_in_dir(char *path, char* goal)
 {
-    char input[100];
+	DIR *directory;
+	struct dirent *dir_entry;
+	struct stat st;
+	char nume[PATH_MAX];
+	int dir;
+	int last_search_res = 0;
 
-    while(true){
-        char* s = fgets(input, sizeof(input), stdin);
-        if (s == NULL)
-            break;
+	dir = is_dir(path);
 
-        if (PARSE_CONDITION("quit")){
-            break;
-        }
-        else if (PARSE_CONDITION("login:")){
-            int return_val = 0;
-            int pipe_d[2];
+	if (dir != 1)
+	{
+		int len = strlen(goal + strlen("mystat "));
+		if (strcmp(path + strlen(path) - len, goal + strlen("mystat ")) == 0 && path[strlen(path) - len - 1] == '/')
+		{
+			char new_path[PATH_MAX];
+			strcat(new_path, "mystat ");
+			strcat(new_path + strlen(new_path), path);
 
-            pipe(pipe_d);
-            pid_t pid = fork();
-            if (pid == -1){
-                error_log("Fork failed");
-                return -1;
-            }
-            else if (pid != 0){
-                wait(&return_val);
+			int res = get_info_func(new_path);
+			if (res != 1)
+				return -1;
+			return 1;
+		}
+	}
+	else
+	{
+		if (NULL == (directory = opendir(path)))
+		{
+			error_log("Unable to open directory");
+			return -1;
+		}
 
-                char buff[20];
-                int len_read = read(pipe_d[READ], buff, 100);
-                buff[len_read] = '\0';
+		while (NULL != (dir_entry = readdir(directory)) && last_search_res == 0)
+		{
+			if ((strcmp(dir_entry->d_name, ".") != 0) && (strcmp(dir_entry->d_name, "..") != 0))
+			{
+				sprintf(nume, "%s/%s", path, dir_entry->d_name);
+				last_search_res = search_in_dir(nume, goal);
+			}
+		}
 
-                char* message_start = check_correct_size(buff);
-                if (!message_start){
-                    error_log("Receive message not complete");
-                    return -1;
-                }
+		closedir(directory);
+	}
 
-                if(memcmp(message_start,"not found", strlen("not found")) == 0){
-                    console_log("Users db not found - login functionality not available");
-                }
-                else if (memcmp(message_start,"success", strlen("success")) == 0) {
-                    console_log("User recognized");
-                    console_log("You are now logged in");
-                }
-                else if (memcmp(message_start,"failed", strlen("failed")) == 0) {
-                    console_log("User not found");
-                }
-                else {
-                    error_log("Subprocess response not recognized");
-                }
-            }
-            else {
-                return login_func(input, pipe_d);
-            }
-        }
-        else if (PARSE_CONDITION("mystat ")){
-            char path[100] = "/home/first/Desktop/RiCa-One";
-            if (getcwd(path, sizeof(path)) == NULL) {
-                error_log("getcwd error");
-                return -1;
-            }
+	return last_search_res;
+}
 
-            char* fifo_name = "fisier.txt";
-            strcat(path + strlen(path), "/");
-            strcat(path + strlen(path), fifo_name);
-            printf("%s\n",path);
+int call_find_func(char* input) {
+	int sockets[2], child;
+	char buf[1024];
 
-            if (-1 == mkfifo(path, 0777)){
-                error_log("Could not create fifo");
-                perror("Descr:");
-                return -1;
-            }
-            pid_t pid = fork();
-            int return_val;
+	socketpair(AF_UNIX, SOCK_STREAM, 0, sockets);
+	child = fork();
 
-            if (pid == -1){
-                error_log("Fork failed");
-                return 0;
-            }
-            else if (pid != 0){
-                wait(&return_val);
+	if (child) { // parent
+		close(sockets[1]);
 
-                char buffer[1000];
-                FILE* fd = fopen(fifo_name, "r");
-                fseek(fd, 0L, SEEK_END);
-                int sz = ftell(fd);
-                fseek(fd, 0L, SEEK_SET);
-                fread(buffer, 1, sz, fd);
+		int res;
 
-                char* message_start = check_correct_size(buffer);
-                if (!message_start){
-                    error_log("Receive message not complete");
-                    return -1;
-                }
+		write(sockets[0], input, strlen(input));
 
-                if (memcmp(message_start,"failed", strlen("failed")) == 0) {
-                    error_log("Stat function failed");
-                }
-                else {
-                    printf("%s", message_start);
-                }
+		read(sockets[0], &res, 4);
 
-                fclose(fd);
-            }
-            else {
-                return mystat_func(input, fifo_name);
-            }
-        }
-    }
+		close(sockets[0]);
 
-    return 0;
+		if (res == 0)
+			console_log("File not found");
+		if (res < 0)
+			return res;
+		return 1;
+
+
+	}
+	else { // child
+		close(sockets[0]);
+
+		read(sockets[1], buf, 1024);
+
+		int res = search_in_dir("..", input);
+
+		write(sockets[1], &res, 4);
+
+		close(sockets[1]);
+	}
+	return 0;
+}
+
+int main()
+{
+	char input[100];
+
+	while (true) {
+		char* s = fgets(input, sizeof(input), stdin);
+		input[strlen(input) - 1] = '\0';
+		if (s == NULL)
+			break;
+
+		if (PARSE_CONDITION("quit")) {
+			break;
+		}
+		else if (PARSE_CONDITION("login:")) {
+			int return_val = 0;
+			int pipe_d[2];
+
+			pipe(pipe_d);
+			pid_t pid = fork();
+			if (pid == -1) {
+				error_log("Fork failed");
+				return -1;
+			}
+			else if (pid != 0) {
+				wait(&return_val);
+
+				char buff[20];
+				int len_read = read(pipe_d[READ], buff, 100);
+				buff[len_read] = '\0';
+
+				struct get_size size = get_message_size(buff);
+				if (strlen(size.newStart) != size.len) {
+					error_log("Receive message not complete");
+					return -1;
+				}
+
+				if (memcmp(size.newStart, "not found", strlen("not found")) == 0) {
+					console_log("Users db not found - login functionality not available");
+				}
+				else if (memcmp(size.newStart, "success", strlen("success")) == 0) {
+					console_log("User recognized");
+					console_log("You are now logged in");
+				}
+				else if (memcmp(size.newStart, "failed", strlen("failed")) == 0) {
+					console_log("User not found");
+				}
+				else {
+					error_log("Subprocess response not recognized");
+				}
+			}
+			else {
+				return login_func(input, pipe_d);
+			}
+		}
+		else if (PARSE_CONDITION("mystat ")) {
+			int res = get_info_func(input);
+			if (res <= 0)
+				return res;
+		}
+		else if (PARSE_CONDITION("cwd")) {
+			char path[100];
+			if (getcwd(path, sizeof(path)) == NULL) {
+				error_log("getcwd error");
+				return -1;
+			}
+			printf("%s\n", path);
+		}
+		else if (PARSE_CONDITION("myfind ")) {
+			int res = call_find_func(input);
+			if (res < 0)
+				return res;
+		}
+		else {
+			printf("Command not recognized\n");
+		}
+	}
+
+	return 0;
 }
